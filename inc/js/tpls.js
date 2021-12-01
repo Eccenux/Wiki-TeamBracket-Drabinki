659b9e7133c4e2d0514a36d7f865e9518836b93d
Nux <eccenux+git@gmail.com>
2021-12-01 03:05:38 +0100

    tpls in separate js

---
+++ inc/js/tpls.js
@@ -0,0 +1,333 @@
+/**
+	Templates for code of TeamBracket.
+	
+	szablon[r] = `template for n-round TeamBracket with empty params`.
+*/
+var szablon = new Array();
+
+//
+// 5 rund (32 zawodników)
+szablon[5] =`
+{{32TeamBracket|
+| RD1 =
+| RD2 =
+| RD3 =
+| RD4 =
+| RD5 =
+
+| group1 =
+| group2 =
+| group3 =
+| group4 =
+
+|  RD1-seed01 = 
+|  RD1-team01 =
+| RD1-score01 =
+|  RD1-seed02 =
+|  RD1-team02 =
+| RD1-score02 =
+|  RD1-seed03 =
+|  RD1-team03 =
+| RD1-score03 =
+|  RD1-seed04 =
+|  RD1-team04 =
+| RD1-score04 =
+|  RD1-seed05 = 
+|  RD1-team05 =
+| RD1-score05 =
+|  RD1-seed06 = 
+|  RD1-team06 =
+| RD1-score06 =
+|  RD1-seed07 =
+|  RD1-team07 =
+| RD1-score07 =
+|  RD1-seed08 =
+|  RD1-team08 =
+| RD1-score08 =
+|  RD1-seed09 =
+|  RD1-team09 =
+| RD1-score09 =
+|  RD1-seed10 =
+|  RD1-team10 =
+| RD1-score10 =
+|  RD1-seed11 =
+|  RD1-team11 =
+| RD1-score11 =
+|  RD1-seed12 =
+|  RD1-team12 =
+| RD1-score12 =
+|  RD1-seed13 =
+|  RD1-team13 =
+| RD1-score13 =
+|  RD1-seed14 =
+|  RD1-team14 =
+| RD1-score14 =
+|  RD1-seed15 =
+|  RD1-team15 =
+| RD1-score15 =
+|  RD1-seed16 =
+|  RD1-team16 =
+| RD1-score16 =
+|  RD1-seed17 =
+|  RD1-team17 =
+| RD1-score17 =
+|  RD1-seed18 =
+|  RD1-team18 =
+| RD1-score18 =
+|  RD1-seed19 =
+|  RD1-team19 =
+| RD1-score19 =
+|  RD1-seed20 =
+|  RD1-team20 =
+| RD1-score20 =
+|  RD1-seed21 =
+|  RD1-team21 =
+| RD1-score21 =
+|  RD1-seed22 =
+|  RD1-team22 =
+| RD1-score22 =
+|  RD1-seed23 =
+|  RD1-team23 =
+| RD1-score23 =
+|  RD1-seed24 =
+|  RD1-team24 =
+| RD1-score24 =
+|  RD1-seed25 =
+|  RD1-team25 =
+| RD1-score25 =
+|  RD1-seed26 =
+|  RD1-team26 =
+| RD1-score26 =
+|  RD1-seed27 =
+|  RD1-team27 =
+| RD1-score27 =
+|  RD1-seed28 =
+|  RD1-team28 =
+| RD1-score28 =
+|  RD1-seed29 =
+|  RD1-team29 =
+| RD1-score29 =
+|  RD1-seed30 =
+|  RD1-team30 =
+| RD1-score30 =
+|  RD1-seed31 =
+|  RD1-team31 =
+| RD1-score31 =
+|  RD1-seed32 =
+|  RD1-team32 =
+| RD1-score32 =
+
+|  RD2-seed01 =
+|  RD2-team01 =
+| RD2-score01 =
+|  RD2-seed02 =
+|  RD2-team02 =
+| RD2-score02 =
+|  RD2-seed03 =
+|  RD2-team03 =
+| RD2-score03 =
+|  RD2-seed04 =
+|  RD2-team04 =
+| RD2-score04 =
+|  RD2-seed05 =
+|  RD2-team05 =
+| RD2-score05 =
+|  RD2-seed06 =
+|  RD2-team06 =
+| RD2-score06 =
+|  RD2-seed07 =
+|  RD2-team07 =
+| RD2-score07 =
+|  RD2-seed08 =
+|  RD2-team08 =
+| RD2-score08 =
+|  RD2-seed09 =
+|  RD2-team09 =
+| RD2-score09 =
+|  RD2-seed10 =
+|  RD2-team10 =
+| RD2-score10 =
+|  RD2-seed11 =
+|  RD2-team11 =
+| RD2-score11 =
+|  RD2-seed12 =
+|  RD2-team12 =
+| RD2-score12 =
+|  RD2-seed13 =
+|  RD2-team13 =
+| RD2-score13 =
+|  RD2-seed14 =
+|  RD2-team14 =
+| RD2-score14 =
+|  RD2-seed15 =
+|  RD2-team15 =
+| RD2-score15 =
+|  RD2-seed16 =
+|  RD2-team16 =
+| RD2-score16 =
+
+|  RD3-seed01 =
+|  RD3-team01 =
+| RD3-score01 =
+|  RD3-seed02 =
+|  RD3-team02 =
+| RD3-score02 =
+|  RD3-seed03 =
+|  RD3-team03 =
+| RD3-score03 =
+|  RD3-seed04 =
+|  RD3-team04 =
+| RD3-score04 =
+|  RD3-seed05 =
+|  RD3-team05 =
+| RD3-score05 =
+|  RD3-seed06 =
+|  RD3-team06 =
+| RD3-score06 =
+|  RD3-seed07 =
+|  RD3-team07 =
+| RD3-score07 =
+|  RD3-seed08 =
+|  RD3-team08 =
+| RD3-score08 =
+
+|  RD4-seed01 =
+|  RD4-team01 =
+| RD4-score01 =
+|  RD4-seed02 =
+|  RD4-team02 =
+| RD4-score02 =
+|  RD4-seed03 =
+|  RD4-team03 =
+| RD4-score03 =
+|  RD4-seed04 =
+|  RD4-team04 =
+| RD4-score04 =
+
+|  RD5-seed01 =
+|  RD5-team01 =
+| RD5-score01 =
+|  RD5-seed02 =
+|  RD5-team02 =
+| RD5-score02 =
+}}
+`;
+
+//
+// 4 rundy (16 zawodników)
+szablon[4] =`
+{{16TeamBracket
+| RD1=
+| RD2=
+| RD3=
+| RD4=
+| group1=
+| group2=
+| group3=
+
+|  RD1-seed01=
+|  RD1-team01=
+| RD1-score01=
+|  RD1-seed02=
+|  RD1-team02=
+| RD1-score02=
+
+|  RD1-seed03=
+|  RD1-team03=
+| RD1-score03=
+|  RD1-seed04=
+|  RD1-team04=
+| RD1-score04=
+
+|  RD1-seed05=
+|  RD1-team05=
+| RD1-score05=
+|  RD1-seed06=
+|  RD1-team06=
+| RD1-score06=
+
+|  RD1-seed07=
+|  RD1-team07=
+| RD1-score07=
+|  RD1-seed08=
+|  RD1-team08=
+| RD1-score08=
+
+|  RD1-seed09=
+|  RD1-team09=
+| RD1-score09=
+|  RD1-seed10=
+|  RD1-team10=
+| RD1-score10=
+
+|  RD1-seed11=
+|  RD1-team11=
+| RD1-score11=
+|  RD1-seed12=
+|  RD1-team12=
+| RD1-score12=
+
+|  RD1-seed13=
+|  RD1-team13=
+| RD1-score13=
+|  RD1-seed14=
+|  RD1-team14=
+| RD1-score14=
+
+|  RD1-seed15=
+|  RD1-team15=
+| RD1-score15=
+|  RD1-seed16=
+|  RD1-team16=
+| RD1-score16=
+
+|  RD2-seed01=
+|  RD2-team01=
+| RD2-score01=
+|  RD2-seed02=
+|  RD2-team02=
+| RD2-score02=
+
+|  RD2-seed03=
+|  RD2-team03=
+| RD2-score03=
+|  RD2-seed04=
+|  RD2-team04=
+| RD2-score04=
+
+|  RD2-seed05=
+|  RD2-team05=
+| RD2-score05=
+|  RD2-seed06=
+|  RD2-team06=
+| RD2-score06=
+
+|  RD2-seed07=
+|  RD2-team07=
+| RD2-score07=
+|  RD2-seed08=
+|  RD2-team08=
+| RD2-score08=
+
+|  RD3-seed01=
+|  RD3-team01=
+| RD3-score01=
+|  RD3-seed02=
+|  RD3-team02=
+| RD3-score02=
+
+|  RD3-seed03=
+|  RD3-team03=
+| RD3-score03=
+|  RD3-seed04=
+|  RD3-team04=
+| RD3-score04=
+
+|  RD4-seed01=
+|  RD4-team01=
+| RD4-score01=
+|  RD4-seed02=
+|  RD4-team02=
+| RD4-score02=
+}}
+`;
